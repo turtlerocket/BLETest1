22cca3a7b8b0428f264479c60d2a2c341e3736b8
turtlerocket <39427470+turtlerocket@users.noreply.github.com>
2024-02-10 13:06:11 -0800

    Initial Commit

---
+++ BLETest1/BLETest1App.swift
@@ -0,0 +1,17 @@
+//
+//  BLETest1App.swift
+//  BLETest1
+//
+//  Created by Benjamin  Dai on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BLETest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLETest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLETest1
+//
+//  Created by Benjamin  Dai on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
