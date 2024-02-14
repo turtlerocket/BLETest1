39d65fc215426cd058332096ad6793b20b507119
turtlerocket <39427470+turtlerocket@users.noreply.github.com>
2024-02-14 13:33:17 -0800

    Major connection of new neomorphic UI and bluetooth implementation.  It all works!!!  Calculation of power and speed STILL not correct.  Need the adjust.  Layout needs improvement too.

--- BLETest1/ContentView.swift
+++ BLETest1/ContentView.swift
@@ -1,106 +1,260 @@
-
-
 import SwiftUI
 
+// Main ContentView
 struct ContentView: View {
-    @StateObject var exerciseBike = ExerciseBike()
-    @State private var currentTime = Date()
-    @State private var isTimerStarted = false
-    
-    // Timer to update current time every minute
-    private let internalTimer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
-    
-    // DateFormatter for current time
-    private var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "h:mm a"
-        return formatter
-    }
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "tortoise")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .padding(.leading, 20)
-                Spacer()
-    //            Text("\(currentTime, formatter: dateFormatter)")
-                    .padding(.trailing, 20)
-            }
-            
-            Spacer()
-            
-            // Display exercise bike information
-            Text("Cadence: \(Int(exerciseBike.cadence)) RPM")
-            Text("Resistance: \(Int(exerciseBike.resistance))")
-            Text("Speed: \(String(format: "%.2f", exerciseBike.speed)) mph")
-            Text("Current Power: \(Int(exerciseBike.power)) Watts")
-            Text("Total Power: \(Int(exerciseBike.totalPower)) Watts")
-            Text("Timer: \(exerciseBike.timerValue) seconds")
-            Text("Total Distance: \(String(format: "%.2f", exerciseBike.totalDistance)) miles")
-            
-            Spacer()
-            
-            // Buttons to control timer and reset values
-            HStack {
-                Button(action: {
-                    isTimerStarted.toggle()
-                    if isTimerStarted {
-                        exerciseBike.startTimer()
-                    } else {
-                        exerciseBike.stopTimer()
-                    }
-                }) {
-                    Text(isTimerStarted ? "Stop Timer" : "Start Timer")
-                        .padding()
-                }
-                
-                Button(action: {
-                    exerciseBike.reset()
-                }) {
-                    Text("Reset")
-                        .padding()
-                }
-            }
-            .padding()
-        }
-        .onReceive(internalTimer) { _ in
-      //      currentTime = Date()
-       //     debugPrint("UPDATED current date time: \(currentTime)")
-            //exerciseBike.updateBikingNumbers()
-        }
-        .onAppear {
-          //  exerciseBike.updateBikingNumbers()
-            
-            // Update cadence and resistance every 2 seconds
-    //        Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
-              //  exerciseBike.updateBikingNumbers()
-        //    }
-        }
-    }
-}
-/*
-struct ContentView: View {
-    @StateObject var exerciseBike = ExerciseBike()
-    
-    var body: some View {
-        VStack {
-            Text("Cadence: \(exerciseBike.cadence)")
-            Text("Resistance: \(exerciseBike.resistance)")
-            Text("Power: \(exerciseBike.power)")
-            Spacer()
-            Text("Current Time: \(exerciseBike.currentTime)")
-                .padding(.top, 20)
-                .padding(.trailing, 20)
-        }
-        .onAppear {
-            exerciseBike.startScan()
-        }
-    }
-}
-*/
+    @StateObject var viewModel = ExerciseBike()
+    @State private var isSpeedDisplayed = true // Toggle between speed and power
+    @State private var widthSize: CGFloat = 10.0 // Declare widthSize as a state variable
 
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack {
+                    Circle()
+                        .fill(Color.black)
+                        .frame(width: 350, height: 350)
+                        .shadow(color: Color.white.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.black.opacity(0.7), radius: 10, x: 5, y: 5)
+                    
+                    GaugeView(value: isSpeedDisplayed ? viewModel.exerciseData.speed : viewModel.exerciseData.currentPower,
+                              minValue: 0,
+                              maxValue: isSpeedDisplayed ? 40 : 1000,
+                              unit: isSpeedDisplayed ? "kph" : "watts") // kph: kilometers per hour, wph: watts per hour
+                        .frame(width: 300, height: 300) // Set size of the gauge
+                        .padding()
+                        .font(.system(size: geometry.size.width * 0.05)) // Adjust font size based on width
+                }
+
+                Spacer()
+
+                SpeedPowerToggle(isSpeedDisplayed: $isSpeedDisplayed)
+                    .padding()
+
+                Spacer()
+
+                LazyVGrid(columns: [
+                    GridItem(alignment: .center),
+                    GridItem(alignment: .leading)
+                ]) {
+                    VStack(alignment: .leading) {
+                        Text("Timer: \(viewModel.exerciseData.formattedTime)")
+                            .font(.system(size: widthSize - 5))
+                            .foregroundColor(Color.white) // Set font color to white
+                        Text("Total Power: \(String(format: "%.2f", viewModel.exerciseData.totalPower))")
+                            .font(.system(size: widthSize - 5))
+                            .foregroundColor(Color.white) // Set font color to white
+                        Text("Total Distance: \(String(format: "%.2f", viewModel.exerciseData.totalDistance))")
+                            .font(.system(size: widthSize - 5))
+                            .foregroundColor(Color.white) // Set font color to white
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.black)
+                            .shadow(color: Color.white.opacity(0.2), radius: 5, x: -2, y: -2)
+                            .shadow(color: Color.black.opacity(0.7), radius: 5, x: 2, y: 2)
+                    )
+
+                    VStack(alignment: .leading) {
+                        Text("Cadence: \(Int(viewModel.exerciseData.cadence))")
+                            .font(.system(size: widthSize - 5))
+                            .foregroundColor(Color.white) // Set font color to white
+                        Text("Resistance: \(Int(viewModel.exerciseData.resistance))")
+                            .font(.system(size: widthSize - 5))
+                            .foregroundColor(Color.white) // Set font color to white
+                        Spacer()
+                        Text("Max Cadence: \(Int(viewModel.exerciseData.maximumCadence))")
+                            .font(.system(size: widthSize - 8)) // Smaller font
+                            .foregroundColor(Color.white) // Set font color to white
+                        Text("Max Power: \(Int(viewModel.exerciseData.maximumPower))")
+                            .font(.system(size: widthSize - 8)) // Smaller font
+                            .foregroundColor(Color.white) // Set font color to white
+                        Text("Max Speed: \(Int(viewModel.exerciseData.maximumSpeed))")
+                            .font(.system(size: widthSize - 8)) // Smaller font
+                            .foregroundColor(Color.white) // Set font color to white
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.black)
+                            .shadow(color: Color.white.opacity(0.2), radius: 5, x: -2, y: -2)
+                            .shadow(color: Color.black.opacity(0.7), radius: 5, x: 2, y: 2)
+                    )
+                }
+                .padding()
+
+                Spacer()
+
+                // Buttons for start/pause and reset
+                HStack {
+                    Spacer()
+                    if viewModel.isTimerRunning {
+                        Button(action: {
+                            viewModel.stopTimer()
+                        }) {
+                            Image(systemName: "pause.fill") // Use system image for Pause action
+                                .font(.system(size: geometry.size.width * 0.03)) // Adjust font size based on width
+                        }
+                        .buttonStyle(NeomorphicButtonStyle(buttonColor: Color.red, fontColor: Color.white)) // Apply neomorphic button style
+                    } else {
+                        Button(action: {
+                            viewModel.startTimer()
+                        }) {
+                            Image(systemName: "play.fill") // Use system image for Start action
+                                .font(.system(size: geometry.size.width * 0.03)) // Adjust font size based on width
+                        }
+                        .buttonStyle(NeomorphicButtonStyle(buttonColor: Color.green, fontColor: Color.white)) // Apply neomorphic button style
+                    }
+
+                    Spacer(minLength: 10).frame(maxWidth: 20) // Add spacer with maximum length of 50
+
+                    Button(action: {
+                        viewModel.resetTimer()
+                    }) {
+                        Text("Reset")
+                            .font(.system(size: geometry.size.width * 0.03)) // Adjust font size based on width
+                    }
+                    .buttonStyle(NeomorphicButtonStyle(buttonColor: Color.black, fontColor: Color.white)) // Apply neomorphic button style
+
+                    Spacer()
+                }
+                .padding()
+            }
+            .background(Color.black.edgesIgnoringSafeArea(.all)) // Set background color to black
+        }
+        .onAppear {
+            updateWidthSize()
+        }
+        .onChange(of: horizontalSizeClass) { _ in
+            updateWidthSize()
+        }
+    }
+
+    private func updateWidthSize() {
+        if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+            widthSize = 20 // Adjust font size for compact width, regular height
+        } else if horizontalSizeClass == .regular && verticalSizeClass == .compact {
+            widthSize = 40
+        } else {
+            widthSize = 30 // Default font size for other size classes
+        }
+    }
+}
+
+struct SpeedPowerToggle: View {
+    @Binding var isSpeedDisplayed: Bool
+
+    var body: some View {
+        HStack {
+            Text("Powermeter")
+                .font(.system(size: 15))
+                .foregroundColor(isSpeedDisplayed ? .gray : .white) // Set font color based on isSpeedDisplayed
+                .padding(.trailing, 15) // Apply trailing padding for Speed label
+
+            Toggle("", isOn: $isSpeedDisplayed)
+                .labelsHidden()
+                .padding(.horizontal, 5) // Adjust horizontal padding for the Toggle button
+
+            Text("Speedometer")
+                .font(.system(size: 15))
+                .foregroundColor(isSpeedDisplayed ? .white : .gray) // Set font color based on isSpeedDisplayed
+                .padding(.leading, 15) // Apply leading padding for Power label
+        }
+        .padding(20) // Apply padding around the HStack
+        .frame(maxWidth: .infinity)
+        .background(Color.black)
+        .cornerRadius(10)
+    }
+}
+
+
+// Define a neomorphic button style
+struct NeomorphicButtonStyle: ButtonStyle {
+    let buttonColor: Color
+    let fontColor: Color
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(buttonColor)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 2, y: 2)
+                    .shadow(color: Color.white.opacity(0.7), radius: 5, x: -2, y: -2)
+            )
+            .foregroundColor(fontColor)
+            .font(.headline)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Add scale effect on press
+    }
+}
+
+
+struct BikeInfoView: View {
+    let title: String
+    let value: Double
+
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.black)
+            Text("\(value)")
+                .font(.body)
+                .foregroundColor(.black)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 2)
+    }
+}
+
+
+struct GaugeView: View {
+    var value: Double
+    var minValue: Double
+    var maxValue: Double
+    var unit: String // Unit of measurement (e.g., kph, watts)
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .trim(from: 0, to: CGFloat(value / maxValue))
+                .stroke(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing), lineWidth: 40)
+                .rotationEffect(.degrees(90))
+                .padding(10)
+            
+            VStack {
+                Text("\(Int(value))")
+                    .font(.system(size: circleFontSize() * 0.30)) // Set font size based on circle dimensions
+                    .fontWeight(.bold)
+                    .foregroundColor(.white) // Set text color to black
+                
+                Text("\(unit)")
+                    .font(.system(size: circleFontSize() * 0.10)) // Set font size to 50% of value font size
+                    .foregroundColor(.white) // Set text color to black
+            }
+        }
+    }
+    
+    // Calculate font size based on the dimensions of the Circle
+    private func circleFontSize() -> CGFloat {
+        let radius = CGFloat(150) // Set the radius of the Circle
+        let circumference = 2 * CGFloat.pi * radius // Calculate the circumference
+        let fontSize = circumference / CGFloat(unit.count) // Adjust font size based on the length of the unit
+        return fontSize
+    }
+}
+
+
+
+
+
+
+// Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- BLETest1/ExerciseBike.swift
+++ BLETest1/ExerciseBike.swift
@@ -3,6 +3,43 @@
 import SwiftUI
 import CoreBluetooth
 
+// Model data for exercise bike
+struct ExerciseBikeData {
+    var speed: Double
+    var cadence: Double
+    var resistance: Double
+    var currentPower: Double
+    var totalPower: Double
+    var totalDistance: Double
+    var elapsedTime: TimeInterval // Elapsed time in seconds
+    
+    // Properties to track maximum values
+    var maximumCadence: Double
+    var maximumSpeed: Double
+    var maximumPower: Double
+    
+    var formattedTime: String {
+        let hours = Int(elapsedTime) / 3600
+        let minutes = Int(elapsedTime) / 60 % 60
+        let seconds = Int(elapsedTime) % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+    
+    init() {
+        self.speed = 0
+        self.cadence = 0
+        self.resistance = 0
+        self.currentPower = 0
+        self.totalPower = 0
+        self.totalDistance = 0
+        self.elapsedTime = 0
+        self.maximumCadence = 0
+        self.maximumSpeed = 0
+        self.maximumPower = 0
+    }
+}
+
+
 class ExerciseBike: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
     private var centralManager: CBCentralManager!
     private var exercisePeripheral: CBPeripheral?
@@ -12,24 +49,117 @@ class ExerciseBike: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeri
     private let notifyCharacteristicUUID1 = CBUUID(string: "0bf669f3-45f2-11e7-9598-0800200c9a66")
     private let notifyCharacteristicUUID2 = CBUUID(string: "0bf669f4-45f2-11e7-9598-0800200c9a66")
     
-    @Published var cadence: UInt16 = 0
-    @Published var resistance: UInt16 = 0
-    @Published var power: UInt16 = 0
-    @Published var speed: UInt16 = 0
-    @Published var currentTime: String = ""
-    
-    @Published var totalPower: UInt16 = 0
-    @Published var timerValue: Int = 0
-    @Published var totalDistance: UInt16 = 0
-    
-    // Timer properties
+    @Published var exerciseData: ExerciseBikeData
     private var timer: Timer?
-    public var isTimerRunning: Bool = false
+    private var startTime: Date?
     
+    @Published var isTimerRunning = false // Track whether the timer is running
+
     override init() {
+        // Initialize with default values
+        self.exerciseData = ExerciseBikeData()
+        
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        // Start updating cadence and resistance every 2 seconds
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            self.updateCadenceAndResistance()
+        }
     }
+    
+    // Update cadence and resistance with realistic bike numbers
+    private func updateCadenceAndResistance() {
+    /*    let cadence = Double.random(in: 60...120) // Realistic cadence range
+        let resistance = Double.random(in: 1...10) // Realistic resistance range
+        
+        // Check if new maximum values are reached and update accordingly
+        if cadence > exerciseData.maximumCadence {
+            exerciseData.maximumCadence = cadence
+        }
+     */
+     
+  //      self.exerciseData.cadence = cadence
+  //      self.exerciseData.resistance = resistance
+        self.exerciseData.currentPower = self.calculatePower(cadence: self.exerciseData.cadence, resistance:  self.exerciseData.resistance)
+        self.exerciseData.speed = self.calculateSpeed(cadence: self.exerciseData.cadence, resistance:  self.exerciseData.resistance)
+      
+        if   self.exerciseData.currentPower > exerciseData.maximumPower {
+            exerciseData.maximumPower = self.exerciseData.currentPower
+        }
+        if  self.exerciseData.speed > exerciseData.maximumSpeed {
+            exerciseData.maximumSpeed =  self.exerciseData.speed
+        }
+    }
+    
+    // Start the timer
+    func startTimer() {
+        guard !isTimerRunning else { return } // Check if timer is already running
+
+        // Start updating data
+        startTime = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            guard let self = self, let startTime = self.startTime else { return }
+            
+            // Calculate the time interval since the start time
+            let currentTime = Date()
+            let elapsedTime = currentTime.timeIntervalSince(startTime)
+
+            // Update data with realistic values
+            let currentSpeed = self.exerciseData.speed
+            let currentPower = currentSpeed * self.exerciseData.resistance
+            self.exerciseData.currentPower = currentPower
+
+            // Update total power and total distance only every second
+            if Int(elapsedTime) % 1 == 0 {
+                self.exerciseData.totalPower += currentPower / 3600 // Power is in Watt-Hours
+                self.exerciseData.totalDistance += currentSpeed / 3600 // Distance is in units per second (e.g., meters/second)
+            }
+
+            // Update elapsed time
+            self.exerciseData.elapsedTime = elapsedTime
+        }
+        
+        isTimerRunning = true
+    }
+
+    // Stop the timer
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        isTimerRunning = false
+    }
+
+    // Reset the timer, total distance, total power, and maximum values
+    func resetTimer() {
+        stopTimer()
+        exerciseData.elapsedTime = 0
+        exerciseData.totalDistance = 0
+        exerciseData.totalPower = 0
+        exerciseData.maximumCadence = 0
+        exerciseData.maximumSpeed = 0
+        exerciseData.maximumPower = 0
+    }
+
+    // Calculate power from cadence and resistance
+    private func calculatePower(cadence: Double, resistance: Double) -> Double {
+        // Typical power calculation: power = cadence * resistance
+        // Typicalthis bike has resistance level to N.m so the formula is Power (kW) = Torque (N.m) x Speed (RPM) / 9.5488
+
+        return (cadence * resistance) / 9.5488
+    }
+    
+    // Calculate speed from cadence and resistance
+    private func calculateSpeed(cadence: Double, resistance: Double) -> Double {
+        // Implement your speed calculation logic here
+        // Example calculation: speed = (cadence * resistance) / 10
+        //   return (cadence * resistance)/10
+        return ((cadence - 35) * 0.4) * ((resistance/100) * 9) + 0.4
+    }
+    
+    
+// BLE code
     
     func startScan() {
         centralManager.scanForPeripherals(withServices: [bikeUUID], options: nil)
@@ -98,71 +228,23 @@ class ExerciseBike: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeri
             if notificationType == 0xD1 {  // Cadence notification
                 print("CADENCE message: \(characteristic): \(data) : \(notificationType)")
                 if data.count >= 11 {
-                    cadence = (UInt16(data[9]) << 8) + UInt16(data[10])
-                    power = getPower(cadence: cadence, resistance: UInt8(resistance))
-                    print("  CADENCE: \(cadence)  resistance \(resistance)  power: \(power)")
+                    self.exerciseData.cadence = Double((Int16(data[9]) << 8) + Int16(data[10]))
+                    self.exerciseData.currentPower = calculatePower(cadence: self.exerciseData.cadence, resistance: Double(self.exerciseData.resistance))
+                    print("  CADENCE: \( self.exerciseData.cadence)  resistance \(self.exerciseData.resistance)  power: \(self.exerciseData.currentPower)")
                 } else {
                     print("Error: Invalid data length for Cadence message")
                 }
             } else if notificationType == 0xD2 {  // Resistance notification
                 print("Resistance message: \(characteristic): \(data) : \(notificationType)")
                 if data.count >= 4 {
-                    resistance = UInt16(data[3])
-                    power = getPower(cadence: cadence, resistance: UInt8(resistance))
-                    print("  RESISTANCE: \(resistance)  cadence: \(cadence) power: \(power)")
+                    self.exerciseData.resistance = Double(data[3])
+                    self.exerciseData.currentPower = calculatePower(cadence: self.exerciseData.cadence, resistance: self.exerciseData.resistance)
+                    print("  RESISTANCE: \(self.exerciseData.resistance)  cadence: \(self.exerciseData.cadence) power: \(self.exerciseData.currentPower)")
                 } else {
                     print("Error: Invalid data length for Resistance message")
                 }
             }
         }
     }
-    
-    func getPower(cadence: UInt16, resistance: UInt8) -> UInt16 {
-        // Implement your power calculation here
-        return cadence * UInt16(resistance)
-    }
-    
-    // Start the timer
-    func startTimer() {
-        guard !isTimerRunning else { return }
-        isTimerRunning = true
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            self.timerValue += 1
-            self.updateValues()
-        }
-    }
-    
-    // Stop the timer
-    func stopTimer() {
-        guard isTimerRunning else { return }
-        isTimerRunning = false
-        timer?.invalidate()
-    }
-    
-    // Reset all values
-    func reset() {
-        stopTimer()
-        timerValue = 0
-        totalDistance = 0
-        totalPower = 0
-    }
-    
-    // Update values based on timer
-    private func updateValues() {
-        speed = (cadence * resistance) / 100
-        power = (cadence * resistance) / 2
-        
-        // Update total distance and power if the timer is running
-        if isTimerRunning {
-            
-            totalDistance += speed / 3600
-            totalPower += power
-            debugPrint("  totalDistance: \(totalDistance)  totalPower \(totalPower)")
-        }
-    }
-    
-
-    
-    
 }
 
