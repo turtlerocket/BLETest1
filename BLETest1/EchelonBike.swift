fb9273ff175242866c0a930a559900a79d6a24a6
turtlerocket <39427470+turtlerocket@users.noreply.github.com>
2024-02-15 18:52:54 -0800

    Added bike loading and error message to Loading Screen.   Added Neomorphic layout for all metrics.

---
+++ BLETest1/EchelonBike.swift
@@ -0,0 +1,8 @@
+//
+//  EchelonBike.swift
+//  BLETest1
+//
+//  Created by Benjamin  Dai on 2/15/24.
+//
+
+import Foundation
---
+++ ExerciseBikeProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseBikeProtocol.swift
+//  BLETest1
+//
+//  Created by Benjamin  Dai on 2/15/24.
+//
+
+import Foundation
