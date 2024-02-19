4ccfc4ddfdfb460e385e60c30dbb53eea8c095d3
turtlerocket <39427470+turtlerocket@users.noreply.github.com>
2024-02-19 11:29:13 -0800

    Refactored name of app from BLETest1 to Simple Spin almost everywhere (except project folder).

--- Simple Spin/SimpleSpinApp.swift
+++ Simple Spin/SimpleSpinApp.swift
@@ -1,14 +1,12 @@
 //
-//  BLETest1App.swift
-//  BLETest1
-//
+//  Simple Spin
 //  Created by Benjamin  Dai on 2/10/24.
 //
 
 import SwiftUI
 
 @main
-struct BLETest1App: App {
+struct SimpleSpinApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
