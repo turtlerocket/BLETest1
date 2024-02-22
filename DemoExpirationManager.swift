202204cf896ae63554cb92438c485a3469eeb596
turtlerocket <39427470+turtlerocket@users.noreply.github.com>
2024-02-21 19:13:32 -0800

    Added subscription key and demo expiration of 3 days with classes to code.  Not yet quite linked to main content view.  Still need to create subscription screen and block app if demo expired with no subscription.

---
+++ DemoExpirationManager.swift
@@ -0,0 +1,8 @@
+//
+//  DemoExpirationManager.swift
+//  Simple Spin
+//
+//  Created by Benjamin  Dai on 2/21/24.
+//
+
+import Foundation
---
+++ KeychainService.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainService.swift
+//  Simple Spin
+//
+//  Created by Benjamin  Dai on 2/21/24.
+//
+
+import Foundation
